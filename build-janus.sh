d8879b91795110ec53d220f044ef4681187ddfec
pascalbaljet <pascal@pascalbaljet.nl>
2017-05-19 17:16:10 +0200

    Update build-janus.sh

--- build-janus.sh
+++ build-janus.sh
@@ -6,6 +6,6 @@ git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
 git checkout ${JANUS_VERSION}
 sh autogen.sh
-./configure --prefix=/usr --disable-rabbitmq --disable-docs --enable-post-processing
+./configure --enable-websockets --disable-data-channels --disable-rabbitmq --disable-mqtt --enable-post-processing
 make
 sudo make install
